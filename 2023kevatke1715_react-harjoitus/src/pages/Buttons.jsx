91b765ace6c3ceeeca265045b805d5b43cffb0f9
Your Name <you@example.com>
2023-02-22 18:20:07 +0200

    router tehtiin

--- 2023kevatke1715_react-harjoitus/src/pages/Buttons.jsx
+++ 2023kevatke1715_react-harjoitus/src/pages/Buttons.jsx
@@ -5,20 +5,16 @@ export const Buttons = ()=>{
 
     const[count, setCount] = useState(0);
     const[count2, setCount2] = useState(0);
-
     function countClicks(){
         setCount(count +1);
     }
-
     function countClicks2(){
         setCount2(count2 +1);
     }
-
     function resetCount(){
         setCount(0);
         setCount2(0);
     }
-
     return(
         <div>
             <button onClick={countClicks}>Clicked {count} times!</button>
