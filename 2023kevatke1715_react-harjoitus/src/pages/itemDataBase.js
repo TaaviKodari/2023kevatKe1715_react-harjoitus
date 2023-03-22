76a57bc35aba40b3b448c6ed6ba5383480fc1d11
Your Name <you@example.com>
2023-03-22 18:14:16 +0200

    lista kesken

--- 2023kevatke1715_react-harjoitus/src/pages/itemDataBase.js
+++ 2023kevatke1715_react-harjoitus/src/pages/itemDataBase.js
@@ -1,14 +1,16 @@
 export const items=[
     {
-        img:"https://cdn.pixabay.com/photo/2023/03/08/20/52/architecture-7838713_1280.jpg",
+        img:"https://cdn.pixabay.com/photo/2023/03/08/20/52/architecture-7838713_1280.jpg"
+        ,
         title:"Architecture",
-        describe:"Jotain arkkitehtuuria",
+        description:"Jotain arkkitehtuuria",
         id: 0,
     },
     {
-        img:"https://cdn.pixabay.com/photo/2022/10/04/14/27/cat-7498364_1280.jpg",
+        img:"https://cdn.pixabay.com/photo/2022/10/04/14/27/cat-7498364_1280.jpg"
+        ,
         title:"Cat",
-        describe:"Kisse here",
+        description:"Kisse here",
         id: 1,
     },
 ]
