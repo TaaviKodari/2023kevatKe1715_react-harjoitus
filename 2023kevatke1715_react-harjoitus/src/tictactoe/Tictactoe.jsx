c84c900ae58d2a1fc1c7a6248e16bdeef2c14f4d
Your Name <you@example.com>
2023-04-19 18:15:22 +0300

    onSquareClick kesken

--- 2023kevatke1715_react-harjoitus/src/tictactoe/Tictactoe.jsx
+++ 2023kevatke1715_react-harjoitus/src/tictactoe/Tictactoe.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import './Tictactoe.css'
 import { Board } from "./Board";
+import { useState } from "react";
+
+const initialBoard=['','','','','','','','',''];
 
 export const TicTacToe =()=>{
+    const[gameState, setGameState]= useState(initialBoard);
+    const[isXTurn, setIsXTurn] = useState(true);
+    
+    const onSquareClick =(index)=>{
+        let strings = Array.from(gameState);
+        if(strings[index] !== ''){
+            return;
+        }
+        
+    }
+
     return(
         <div>
             <h1>Tic-Tac-Toe</h1>
-            <Board/>
+            <Board gameState={gameState}/>
         </div>
     );
 }
