dd39877fa6b437c82a5403c0c6905548c0a75051
Your Name <you@example.com>
2023-04-26 18:17:01 +0300

    checkWinner tehty

--- 2023kevatke1715_react-harjoitus/src/tictactoe/Tictactoe.jsx
+++ 2023kevatke1715_react-harjoitus/src/tictactoe/Tictactoe.jsx
@@ -14,13 +14,37 @@ export const TicTacToe =()=>{
         if(strings[index] !== ''){
             return;
         }
-        
+        strings[index] = isXTurn ? 'X' : 'O';
+        setGameState(strings);
+        setIsXTurn(!isXTurn);
+    }
+
+    const checkWinner = ()=>{
+        const lines = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6],
+        ];
+        for (let i = 0; i < lines.length; i++){
+            const [a,b,c] = lines[i];
+            if(gameState[a] && gameState[a] === gameState[b] &&
+                 gameState[a] === gameState[c])
+                 {
+                    return gameState[a];
+                 }
+        }
+        return null;
     }
 
     return(
         <div>
             <h1>Tic-Tac-Toe</h1>
-            <Board gameState={gameState}/>
+            <Board gameState={gameState} onSquareClick={onSquareClick}/>
         </div>
     );
 }
